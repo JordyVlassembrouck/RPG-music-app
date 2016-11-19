93578d2228bd7e9fdc1e03b26914aacefd6ed6ce
Jordy Vlassembrouck <jordy.vlassembrouck@aca-it.be>
2016-11-19 15:58:31 +0100

    add more button with menu list and update README

--- app/music/view/music.view.component.ts
+++ app/music/view/music.view.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MdIconRegistry }   from '@angular/material';
 
+
 import { MusicService }     from '../music.service';
 import { Track }            from '../music.track';
 
