cc8b67a74dffa83bf9c9f872b86df974f9d46dd9
Jordy Vlassembrouck <jordy.vlassembrouck@aca-it.be>
2016-11-20 14:40:25 +0100

    update directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,25 @@ var runSequence = require('run-sequence');
 
 
 gulp.task('clean.dev', function() {
-  return del(['../dev'], {force: true});
+  return del(['dev'], {force: true});
 });
 
 gulp.task('copy.html.to.dev', function() {
-  return gulp.src('../app/**/*.html', {base: '../app'})
-    .pipe(gulp.dest('../dev'));
+  return gulp.src('app/**/*.html', {base: 'app'})
+    .pipe(gulp.dest('dev'));
 });
 
 gulp.task('copy.css.to.dev', function() {
-  return gulp.src('../app/**/*.css', {base: '../app'})
-    .pipe(gulp.dest('../dev'));
+  return gulp.src('app/**/*.css', {base: 'app'})
+    .pipe(gulp.dest('dev'));
 });
 
 gulp.task('copy.images.to.dev', function() {
-  return gulp.src('../app/**/*.svg', {base: '../app'})
-    .pipe(gulp.dest('../dev'));
+  return gulp.src('app/**/*.svg', {base: 'app'})
+    .pipe(gulp.dest('dev'));
 });
 
-gulp.task('copy.assets.dev', function(done) {
+gulp.task('copy.resources.dev', function(done) {
   runSequence('copy.html.to.dev', 'copy.css.to.dev', 'copy.images.to.dev', function() {
     done();
   })
